44d0a1b02736dedf8e7e33342dcfbe38d7c39b3d
MARI <mari20201101@gmail.com>
2021-08-23 18:49:51 +0900

    Add member repositorylmpl class

--- src/main/java/com/mari/querydsl/repository/MemberRepositoryCustom.java
+++ src/main/java/com/mari/querydsl/repository/MemberRepositoryCustom.java
@@ -2,10 +2,14 @@ package com.mari.querydsl.repository;
 
 import com.mari.querydsl.dto.MemberApiDto;
 import com.mari.querydsl.dto.MemberSearchCondition;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
 public interface MemberRepositoryCustom {
 
     List<MemberApiDto> search(MemberSearchCondition condition);
+    Page<MemberApiDto> searchSimplePage(MemberSearchCondition condition, Pageable pageable);
+    Page<MemberApiDto> searchComplexPage(MemberSearchCondition condition, Pageable pageable);
 }
--- src/main/java/com/mari/querydsl/repository/MemberRepositoryImpl.java
+++ src/main/java/com/mari/querydsl/repository/MemberRepositoryImpl.java
@@ -3,9 +3,13 @@ package com.mari.querydsl.repository;
 import com.mari.querydsl.dto.MemberApiDto;
 import com.mari.querydsl.dto.MemberSearchCondition;
 import com.mari.querydsl.dto.QMemberApiDto;
+import com.querydsl.core.QueryResults;
 import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
@@ -34,6 +38,60 @@ public class MemberRepositoryImpl implements MemberRepositoryCustom{
                         ageGoe(condition.getGoeAge()),
                         ageLoe(condition.getLoeAge()))
                 .fetch();
+    }
+
+    @Override
+    public Page<MemberApiDto> searchSimplePage(MemberSearchCondition condition, Pageable pageable) {
+        QueryResults<MemberApiDto> result = queryFactory
+                .select(new QMemberApiDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamname")
+                ))
+                .from(member)
+                .join(member.team, team)
+                .where(usernameEq(condition.getUsername()),
+                        teamnameEq(condition.getTeamname()),
+                        ageGoe(condition.getGoeAge()),
+                        ageLoe(condition.getLoeAge()))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetchResults();
+
+        return new PageImpl<>(result.getResults() , pageable,result.getTotal());
+    }
+
+    @Override
+    public Page<MemberApiDto> searchComplexPage(MemberSearchCondition condition, Pageable pageable) {
+
+        List<MemberApiDto> result = queryFactory
+                .select(new QMemberApiDto(
+                        member.id.as("memberId"),
+                        member.username,
+                        member.age,
+                        team.id.as("teamId"),
+                        team.name.as("teamname")
+                ))
+                .from(member)
+                .join(member.team, team)
+                .where(usernameEq(condition.getUsername()),
+                        teamnameEq(condition.getTeamname()),
+                        ageGoe(condition.getGoeAge()),
+                        ageLoe(condition.getLoeAge()))
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
+                .fetch();
+
+        long total = queryFactory
+                .selectFrom(member)
+                .where(usernameEq(condition.getUsername()),
+                teamnameEq(condition.getTeamname()),
+                ageGoe(condition.getGoeAge()),
+                ageLoe(condition.getLoeAge()))
+                .fetchCount();
+        return new PageImpl<>(result , pageable, total);
     }
 
     private BooleanExpression ageLoe(Integer loeAge) {
